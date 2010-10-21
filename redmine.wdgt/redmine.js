218ff43a60d9a88cee57c2d2a501f7e8fd7b9c92
4pcbr <i4pcbr@gmail.com>
2010-10-21 09:41:56 +0400

    fixed update system

--- redmine.wdgt/redmine.js
+++ redmine.wdgt/redmine.js
@@ -381,7 +381,7 @@ RedmineWidget.prototype = {
   update: function() {
     info('updating...');
     var self = this;
-    widget.system('curl "' + DOWNLOAD_NEW_VERSION_URL + '" > /tmp/redmine.wdgt.tar && cd /tmp/ && tar -xf redmine.wdgt.tar && rm redmine.wdgt.tar && mv redmine.wdgt ~/Library/Widgets/', 
+    var _o = widget.system('curl "' + DOWNLOAD_NEW_VERSION_URL + '" > /tmp/redmine.wdgt.tar && cd /tmp/ && tar -xf redmine.wdgt.tar && rm redmine.wdgt.tar && cp -r redmine.wdgt ~/Library/Widgets/ && rm -r redmine.wdgt', 
       function() {
         info('Update complete. Reload this widget with ⌘R for changes to take a place.');
         self._checkForUpdate(function(_res) { cfg('last_release_timestamp', _res.repository.pushed_at) })
